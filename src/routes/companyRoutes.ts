caec5182ddcc97d0a56ceed2eb2756fdbaa95561
Tanjimul Islam <tanjimulislam118@gmail.com>
2023-04-22 11:02:20 +0600

    device routes

---
+++ src/routes/companyRoutes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import { createCompany, getAllCompanies, getCompanyById, updateCompany, deleteCompany } from '../controllers/companyController'
+
+const router = Router()
+
+router.post('/', createCompany)
+router.get('/', getAllCompanies)
+router.get('/:id', getCompanyById)
+router.put('/:id', updateCompany)
+router.delete('/:id', deleteCompany)
+
+export default router
