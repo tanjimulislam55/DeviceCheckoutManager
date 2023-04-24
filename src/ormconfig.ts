9ba88de6f8ead791f8970cfa4af08a46b6ec6d80
Tanjimul Islam <tanjimulislam118@gmail.com>
2023-04-25 00:13:49 +0600

    fixed entities but not migrations

---
+++ src/ormconfig.ts
@@ -0,0 +1,23 @@
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+require('dotenv').config()
+
+import Company from './entities/Company'
+import Device from './entities/Device'
+import DeviceLog from './entities/DeviceLog'
+import Employee from './entities/Employee'
+
+const AppDataSource = new DataSource({
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: 3306,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: true,
+    logging: true,
+    entities: [Company, Device, DeviceLog, Employee],
+    // migrations: ['.migrations/*.ts'],
+})
+
+export default AppDataSource
