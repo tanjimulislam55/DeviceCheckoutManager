f9c1149f0ac2319800f4e97750ee30c3feaa8c95
Tanjimul Islam <tanjimulislam118@gmail.com>
2023-04-25 00:04:14 +0600

    fixed create api and added lazily loaded

--- src/controllers/employeeController.ts
+++ src/controllers/employeeController.ts
@@ -1,12 +1,29 @@
 import { Request, Response } from 'express'
 
-import { employeeRepository } from '../entities'
+import { companyRepository, employeeRepository } from '../entities'
 
 export const createEmployee = async (req: Request, res: Response): Promise<void> => {
     try {
-        const employee = employeeRepository.create(req.body)
-        const results = await employeeRepository.save(employee)
-        res.status(201).json(results)
+        const existingEmployee = await employeeRepository.findOne({ where: { name: req.body.name } })
+        if (existingEmployee) {
+            res.status(403).json({ message: 'Name already exists' })
+            return
+        }
+        const companyId = req.body.companyId
+        if (!companyId) {
+            res.status(403).json({ message: 'Company id missing' })
+            return
+        } else {
+            const company = await companyRepository.findOneBy({ id: companyId })
+            if (!company) {
+                res.status(404).json({ message: `Company not found with id ${companyId}` })
+                return
+            } else {
+                const employee = employeeRepository.create(req.body)
+                const results = await employeeRepository.save(employee)
+                res.status(201).json(results)
+            }
+        }
     } catch (err) {
         console.error(err)
         res.status(500).json({ message: 'Server Error' })
@@ -27,6 +44,7 @@ export const getEmployeeById = async (req: Request, res: Response): Promise<void
     try {
         const id = parseInt(req.params.id)
         const employee = await employeeRepository.findOneBy({ id: id })
+        await employee?.logs
         if (employee) {
             res.status(200).json(employee)
         } else {
