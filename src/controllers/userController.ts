d0f4feaec1c048c84a10cfb6c145d09b5c770cd4
Tanjimul Islam <tanjimulislam118@gmail.com>
2023-04-26 22:50:58 +0600

    signup, signin included here

---
+++ src/controllers/userController.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from 'express'
+
+import { userRepository } from '../entities'
+import { comparePassword, createToken, encryptPassword } from '../utils/auth'
+
+export const signup = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { email, password } = req.body
+        const existingUser = await userRepository.findOne({ where: { email: email } })
+        if (existingUser) res.status(403).json('Email already in use')
+        else {
+            const hashedPassword = await encryptPassword(password)
+            const user = userRepository.create({ email: email, password: hashedPassword, isSuperUser: false })
+            const results = await userRepository.save(user)
+            res.status(201).json(results)
+        }
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ message: 'Server Error' })
+    }
+}
+
+export const signin = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { email, password } = req.body
+        if (!email || !password) {
+            res.status(403).json('Fields required for email and password')
+            return
+        }
+        const user = await userRepository.findOne({ where: { email: email } })
+        if (!user) {
+            res.status(404).json({ message: 'User not found' })
+            return
+        }
+
+        const isVerified = await comparePassword(user.password, password)
+        if (!isVerified) {
+            res.status(403).json({ message: 'Incorrect password' })
+            return
+        } else {
+            const jwt = createToken(user, 'ACCESS_TOKEN')
+            res.status(200).cookie('accessToken', jwt).json({
+                success: true,
+                status: 'green',
+                user: user,
+            })
+        }
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ message: 'Server Error' })
+    }
+}
+
+export const getAllUsers = async (_req: Request, res: Response): Promise<void> => {
+    try {
+        const users = await userRepository.find()
+        res.status(200).json(users)
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ message: 'Server Error' })
+    }
+}
