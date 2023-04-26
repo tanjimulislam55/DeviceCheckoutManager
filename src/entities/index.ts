636c16115048df96c410a76354e09769062e3e22
Tanjimul Islam <tanjimulislam118@gmail.com>
2023-04-26 22:42:42 +0600

    for new user table

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -3,8 +3,10 @@ import Employee from './Employee'
 import Device from './Device'
 import DeviceLog from './DeviceLog'
 import AppDataSource from '../ormconfig'
+import User from './User'
 
 export const companyRepository = AppDataSource.getRepository(Company)
 export const employeeRepository = AppDataSource.getRepository(Employee)
 export const deviceRepository = AppDataSource.getRepository(Device)
 export const deviceLogRepository = AppDataSource.getRepository(DeviceLog)
+export const userRepository = AppDataSource.getRepository(User)
