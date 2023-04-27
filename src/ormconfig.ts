68d4db98af4dbf54ced62965b51582bb875f64d3
Tanjimul Islam <tanjimulislam118@gmail.com>
2023-04-27 08:13:49 +0600

    entities updated with User

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -6,6 +6,7 @@ import Company from './entities/Company'
 import Device from './entities/Device'
 import DeviceLog from './entities/DeviceLog'
 import Employee from './entities/Employee'
+import User from './entities/User'
 
 const AppDataSource = new DataSource({
     type: 'mysql',
@@ -16,7 +17,7 @@ const AppDataSource = new DataSource({
     database: process.env.DB_NAME,
     synchronize: true,
     logging: true,
-    entities: [Company, Device, DeviceLog, Employee],
+    entities: [Company, Device, DeviceLog, Employee, User],
     // migrations: ['.migrations/*.ts'],
 })
 
