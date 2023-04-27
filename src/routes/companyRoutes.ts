0a08566a4f7df4b1c46601930cfe26d946dadf56
Tanjimul Islam <tanjimulislam118@gmail.com>
2023-04-27 12:05:08 +0600

    auth protected url

--- src/routes/companyRoutes.ts
+++ src/routes/companyRoutes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express'
 
 import { createCompany, getAllCompanies, getCompanyById, updateCompany, deleteCompany } from '../controllers/companyController'
+import { getAuthenticatedUser, getAuthenticatedSuperUser } from '../middlewares/auth'
 
 const router = Router()
 
-router.post('/', createCompany)
-router.get('/', getAllCompanies)
-router.get('/:id', getCompanyById)
-router.put('/:id', updateCompany)
-router.delete('/:id', deleteCompany)
+router.post('/', getAuthenticatedSuperUser, createCompany)
+router.get('/', getAuthenticatedUser, getAllCompanies)
+router.get('/:id', getAuthenticatedUser, getCompanyById)
+router.put('/:id', getAuthenticatedSuperUser, updateCompany)
+router.delete('/:id', getAuthenticatedSuperUser, deleteCompany)
 
 export default router
