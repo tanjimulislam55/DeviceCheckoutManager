d86a3324923463e41b7133e0fc6ce381274d8f55
Tanjimul Islam <tanjimulislam118@gmail.com>
2023-04-28 00:41:34 +0600

    unnecessary imported module removed

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,6 +1,4 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express'
-import { ParamsDictionary } from 'express-serve-static-core'
-import { ParsedQs } from 'qs'
 import jwt from 'jsonwebtoken'
 require('dotenv').config()
 
