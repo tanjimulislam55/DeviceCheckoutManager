4dde701f3352c32feec04c5121e3aff4ecb24ae9
Tanjimul Islam <tanjimulislam118@gmail.com>
2023-04-27 12:32:00 +0600

    auth protected url

--- src/routes/deviceCheckoutRoutes.ts
+++ src/routes/deviceCheckoutRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 
 import { checkoutDevice, checkinDevice } from '../controllers/deviceCheckoutController'
+import { getAuthenticatedUser } from '../middlewares/auth'
 
 const router = Router()
 
-router.post('/checkout', checkoutDevice)
-router.put('/checkin/:deviceId', checkinDevice)
+router.post('/checkout', getAuthenticatedUser, checkoutDevice)
+router.put('/checkin/:deviceId', getAuthenticatedUser, checkinDevice)
 
 export default router
