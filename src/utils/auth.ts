0df8af39c34bd0b89d99ab3a2fc973b8db96e695
Tanjimul Islam <tanjimulislam118@gmail.com>
2023-04-27 09:44:10 +0600

    bug fixed

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -9,7 +9,7 @@ const saltRounds = 10
 export const createToken = (user: User, type: string) => {
     switch (type) {
         case 'ACCESS_TOKEN':
-            return jwt.sign(user.email, process.env.SECRET_KEY!, { algorithm: 'RS256', expiresIn: '7d' })
+            return jwt.sign({ email: user.email }, process.env.SECRET_KEY!, { algorithm: 'HS256', expiresIn: '7d' })
         case 'REFRESH_TOKEN':
             break
         default:
@@ -17,6 +17,6 @@ export const createToken = (user: User, type: string) => {
     }
 }
 
-export const comparePassword = async (hashedPassword: string, plainPassword: string) => await bcrypt.compare(hashedPassword, plainPassword)
+export const comparePassword = async (hashedPassword: string, plainPassword: string) => await bcrypt.compare(plainPassword, hashedPassword)
 
 export const encryptPassword = async (plainPassword: string) => await bcrypt.hash(plainPassword, saltRounds)
