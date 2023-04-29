b956af88598c6ff186af6c50baf6906baf13435b
Tanjimul Islam <tanjimulislam118@gmail.com>
2023-04-29 19:43:26 +0600

    RequestHandler is no longer needed

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction, RequestHandler } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
 require('dotenv').config()
 
